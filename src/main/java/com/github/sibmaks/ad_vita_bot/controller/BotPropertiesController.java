453bfcfee9c439834376f15d32c6ea0acf6c8275
sibmaks <sibmaks@vk.com>
2023-11-15 21:09:20 +0700

    Clean up API

--- src/main/java/com/github/sibmaks/ad_vita_bot/controller/BotPropertiesController.java
+++ src/main/java/com/github/sibmaks/ad_vita_bot/controller/BotPropertiesController.java
@@ -1,10 +1,12 @@
 package com.github.sibmaks.ad_vita_bot.controller;
 
 import com.github.sibmaks.ad_vita_bot.api.rq.UpdatePropertiesRq;
-import com.github.sibmaks.ad_vita_bot.api.rs.GetPropertiesRs;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author sibmaks
@@ -13,12 +15,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/properties/")
 public class BotPropertiesController {
-
-    @GetMapping(path = "all", produces = MediaType.APPLICATION_JSON_VALUE)
-    public GetPropertiesRs getProperties() {
-        // accept {@link CommonConst#HEADER_SESSION_ID} as auth header
-        return new GetPropertiesRs();
-    }
 
     @PostMapping(path = "update", produces = MediaType.APPLICATION_JSON_VALUE)
     public void update(@RequestBody @Validated UpdatePropertiesRq rq) {
